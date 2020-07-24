eb0e5abbd2fdfedb47b27918e6efe11a96920213
luanalbert <contatoluanalbert@gmail.com>
2020-07-24 14:05:54 -0300

    ainda podemser feitas melhorias

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,4 +11,31 @@ function escrever(str, el) {
 }
 escrever(texto, div);
 
+var div = document.getElementById('text-inicio');
+var texto = 'Hello Word!';
+
+function escrever(str, el) {
+  var char = str.split('').reverse();
+  var typer = setInterval(function() {
+    if (!char.length) return clearInterval(typer);
+    var next = char.pop();
+    el.innerHTML += next;
+  }, 100);
+}
+escrever(texto, div);
+
+var div = document.getElementById('sobre');
+var texto = 'Sobre mim!';
+
+function escrever(str, el) {
+  var char = str.split('').reverse();
+  var typer = setInterval(function() {
+    if (!char.length) return clearInterval(typer);
+    var next = char.pop();
+    el.innerHTML += next;
+  }, 100);
+}
+escrever(texto, div);
+
+
 
